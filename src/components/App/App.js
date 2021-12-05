bd80781887310828cb52b102e1b5e6178919f20f
unknown <dave8git@gmail.com>
2021-12-05 20:21:27 +0100

    fix problem with lint installation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import styles from './App.scss';
+import List from '../List/List';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
-      </div>
+      <main className={styles.component}>
+      <h1 className={styles.title}>{pageContents.title}</h1>
+      <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+      {/*<List title={['Rzeczy do zrobienia', <sup key='1'>soon!</sup>]} image={'https://images.pexels.com/photos/6746077/pexels-photo-6746077.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'}>
+        <p>Planuje wykonać te rzeczy szybciej niż później!</p>
+
+    </List>*/}
+    <List {...listData} />
+      </main>
     )
+    
   }
 }
 
